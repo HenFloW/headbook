18f4eb2c5902f5ea92bfac51cc40c8ecd4f0c0aa
Henrik Flo Wilhelmsen <henflowil01@gmail.com>
2023-10-26 21:57:19 +0200

    2c (fix): better validation

--- headbook/signup_form.py
+++ headbook/signup_form.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, HiddenField, validators
 
-
 class SignupForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=3, max=25, message='Username must be between 3 and 25 characters long')])
     password = PasswordField('Password', [
-        validators.equal_to('password_again', message='Passwords must match'), 
-        validators.Length(min=8, max=-1, message='Password must be minimum 8 characters long'), 
-        validators.Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$', message='Password must contain at least one lowercase letter, one uppercase letter, one digit and one special character')])
+        validators.equal_to('password_again', message='Passwords must match'),
+        validators.Length(min=8, max=-1, message='be minimum 8 characters long'), 
+        validators.Regexp('^(?=.*[a-z]).+$', message='contain at least one lowercase letter'),
+        validators.Regexp('^(?=.*[A-Z]).+$', message='contain at least one uppercase letter'),
+        validators.Regexp('^(?=.*\d).+$', message='contain at least one digit'),
+        validators.Regexp('^(?=.*(_|[^\w])).+$', message='contain at least one special character')])
     password_again = PasswordField('Repeat Password')
     signup = SubmitField('Signup')
     next = HiddenField()
